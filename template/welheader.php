60d6cd004b5297e538ce63f3dc826b8bc3230e78
farahaff <farahaff@MacBook-Pro-Farah.local>
2018-03-16 19:41:34 +0800

    test logo2

--- template/welheader.php
+++ template/welheader.php
@@ -36,7 +36,7 @@ unset($_SESSION["logoutmsg"]);
       //-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="img/newlogo.png" height="100px"; width="150px";></a>
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="img/newlogo.png" height="100px"; width="200px";></a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fa fa-bars"></i>
